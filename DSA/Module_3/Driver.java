3d7f4dd4cb7260041074176ee1ff0287d8bcddfa
nafeezurc <nafeezurc@gmail.com>
2025-08-12 23:54:47 -0500

    module 3 additions

--- DSA/Module_3/Driver.java
+++ DSA/Module_3/Driver.java
@@ -3,9 +3,11 @@ import java.util.Arrays;
 public class Driver {
     
     public static void main(String[] args) {
+        
         ArrayQueue<Integer> aq1 = new ArrayQueue<>();
         System.out.println("Initial size = " + aq1.size());
         
+        /*
         aq1.enqueue(0);
         System.out.println("After adding 0: " + aq1.size());
         System.out.println(Arrays.toString(aq1.getBackingArray()));
@@ -20,9 +22,9 @@ public class Driver {
         aq1.enqueue(8);
         System.out.println("After filling to initial capacity: " + aq1.size());
         System.out.println(Arrays.toString(aq1.getBackingArray()));
+        
 
-
-        /*
+        
         Integer x = aq1.dequeue();
         System.out.println("After removing " + x + " from queue: " + aq1.size());
         System.out.println(Arrays.toString(aq1.getBackingArray()));
@@ -49,8 +51,9 @@ public class Driver {
         aq1.dequeue();
         System.out.println("After removing the last element: " + aq1.size());
         System.out.println(Arrays.toString(aq1.getBackingArray()));
-
         */
+        
+        /*
         aq1.enqueue(9);
         System.out.println("After going past initial capacity: " + aq1.size());
         System.out.println(Arrays.toString(aq1.getBackingArray()));
@@ -66,6 +69,26 @@ public class Driver {
         aq1.dequeue();
         aq1.dequeue();
         System.out.println("Clearing: " + aq1.size());
+        System.out.println(Arrays.toString(aq1.getBackingArray()));
+        */
+        aq1.enqueue(0);
+        aq1.enqueue(0);
+        aq1.enqueue(0);
+        aq1.dequeue();
+        aq1.dequeue();
+        aq1.dequeue();
+        System.out.println("Trying failed test case: " + aq1.size());
+        System.out.println(Arrays.toString(aq1.getBackingArray()));
+
+        aq1.enqueue(0);
+        aq1.enqueue(1);
+        aq1.enqueue(2);
+        aq1.enqueue(3);
+        aq1.enqueue(4);
+        aq1.enqueue(5);
+        aq1.enqueue(6);
+        aq1.enqueue(7);
+        aq1.enqueue(8);
         System.out.println(Arrays.toString(aq1.getBackingArray()));
         
     }
